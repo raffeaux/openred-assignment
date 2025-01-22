0a02593eb839dfa7cb3ad163ebfbfe7403f41382
raffeaux <raffaeledicarlo95@gmail.com>
2025-01-22 23:57:43 +0100

    Added two small methods to check API access

--- main.py
+++ main.py
@@ -1,8 +1,24 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 import os
 
 app = FastAPI()
 
+class Request(BaseModel):
+    body: str
+
 @app.get("/")
 async def root():
     return {"message": os.listdir(os.getcwd())}
+
+@app.post("/start")
+async def start_pipeline(body: Request):
+    text = body.body
+    with open(os.path.join(os.getcwd(), "raw"), "w") as fp:
+        fp.write(text)
+
+@app.get("/check")
+async def check_result():
+    with open(os.path.join(os.getcwd(), "raw"), "r") as fp:
+        text = fp.read()
+        return {"message": text}
