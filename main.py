69b63955331a45f344800dbfd8fad8165217b282
raffeaux <raffaeledicarlo95@gmail.com>
2025-01-28 00:07:21 +0100

    Added option to insert new data on last table, with validity check

--- main.py
+++ main.py
@@ -87,6 +87,70 @@ async def start_pipeline(args: Request):
     #finally we send a final webhook to make sure the pipeline is finished
     utils.sendWebhook(dbpath)
 
+@app.post("/insert")
+async def insert_pipeline(args: Request):
+
+    #writing raw data to bucket
+    body = args.bod
+    datapath = os.path.join(os.getcwd(), "data/raw/new_entries/raw_data_" + str(datetime.now()) + ".csv")
+    dqpath = os.path.join(os.getcwd(), "data/data_quality/dq_log_" + str(datetime.now()) + ".txt")
+    with open(datapath, "w") as fp:
+        fp.write(body)
+
+    #we first check if the schema is compatible by comparing the columns in the
+    #new entry with the columns in the master
+
+    last_file = os.listdir("data/raw")[-1]
+    columnSchema = list(pandas.read_csv(last_file))
+
+    if list(pandas.read_csv(datapath)) == columnSchema:
+
+        #running data quality
+        complete = dataQuality.runDataQuality(datapath, dqpath)
+
+        fmpath = os.path.join(os.getcwd(), "data/feature_mining/fm_log_" + str(datetime.now()) + ".txt")
+
+        #running feature mining pipeline
+        mined = featureMining.runFeatureMining(complete, fmpath)
+
+        #now we remove the original columns except address, price, description, 
+        #living_area_m2, plot_area_m2 and volume_m3
+        keep = ["address", "price", "description", "living_area_m2", "plot_area_m2", "volume_m3"]
+        columnNames = [x for x in list(pandas.read_csv(datapath)) if x not in keep]
+
+        clean = mined.drop(columnNames, axis=1)
+
+        dbpath = os.path.join(os.getcwd(), "data/db/db_log_" + str(datetime.now()) + ".txt")
+
+        #now is the time to send all this data to the database
+        #first we specify the connection
+        engine = create_engine(os.environ["SQL_ACCESSKEY"])
+        inspector = inspect(engine)
+        last_table = inspector.get_table_names()[-1]
+
+        with engine.connect() as conn:
+
+            dbTransactions.bulk_insert(clean, last_table, conn)
+
+            conn.close()
+
+            with open(dbpath, "w") as report:
+                report.write("Insert made on {} on {}.".format(last_table, datetime.now()))
+
+        #finally we send a final webhook to make sure the pipeline is finished
+        utils.sendWebhook(dbpath)
+
+    else:
+
+        with open(dqpath, "w") as report:
+            report.write("Mismatching data sent on {}.".format(datetime.now()))
+
+        utils.sendWebhook(dqpath)
+
+        raise KeyError("The column names do not match the schema!")
+
+
+
 @app.get("/check")
 async def check_result():
     with open(os.path.join(os.getcwd(), "data/test.txt"), "r") as fp:
